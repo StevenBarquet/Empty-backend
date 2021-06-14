81eb4992042b13ba98f503f351ee8037b9f76671
Steven Barquet <mlmjm123@gmail.com>
2021-06-14 18:04:27 -0500

    improved eslint and structure

--- index.js
+++ index.js
@@ -1,60 +1,43 @@
 // --------------------------------------IMPORTS------------------------------------
 // Dependencies
 const express = require('express');
-const config = require('config');
-const mongoose = require('mongoose');
-const debugProd=require('debug')('app:prod')
+const debugProd = require('debug')('app:prod')
 // Middlewares
 const helmet = require('helmet');
 const cors = require('cors');
 // Otros
-const startLogs= require('./configuration/startLogs')
-
+const startLogs = require('./configuration/startLogs')
+const mongoConnect = require('./configuration/mongoConfig')
+const getCerts = require('./configuration/getCerts')
 // -----------------------------------CONFIG-------------------------------
 const app = express();
-const enviroment= process.env.NODE_ENV || 'development'
+const enviroment = process.env.NODE_ENV || 'development'
 const port = process.env.PORT || 4000
 
-// Mongo conect to base
-mongoose.connect(config.get('mongoDB'), { useNewUrlParser: true, useFindAndModify:true, useUnifiedTopology: true }) // return a promise
-  .then(()=>debugProd(`'Conected to ${config.get('mongoDB')}...`))
-  .catch(err=> debugProd('Couldnt connect because:\n', err))
-
+mongoConnect();
 startLogs(enviroment); // Just and example of posible use of configs
 
 // -----------------------------------MIDDLEWARES-------------------------------
-app.use(express.json()); // needed to read req.body
-app.use(helmet()); // for security
-app.use(cors()); // for security
+app.use(express.json()); // Needed to read req.body
+app.use(helmet()); // For security
+app.use(cors()); // For security
 
 // -----------------------------------ROUTES-------------------------------
-// app.use('/laptops/', productRoutes)
-// app.use('/ordenes', ordersRoutes)
+// App.use('/laptops/', productRoutes)
+// App.use('/ordenes', ordersRoutes)
 
 // -----------------------------------SSL-------------------------------
 const http = require('http');
 const https = require('https');
-const fs = require('fs');
-
-let optionsSSL
-
-try {
-  optionsSSL = {
-    key: fs.readFileSync('./certificates/certificate.key'),
-    cert: fs.readFileSync('./certificates/certificate.crt')
-  };
-} catch (error) {
-  optionsSSL = {};
-}
 
 const trySSL = process.env.USE_SSL || false // Set use of https from enviroment
 
 const server = trySSL ? https : http
-const options = trySSL ? optionsSSL : {}
+const options = trySSL ? getCerts() : {}; // Get ssl certs if https true
 
 // -----------------------------------SERVER-------------------------------
 server
   .createServer(options, app)
   .listen(port, () => {
-    debugProd('https ', trySSL, ' listening to port ' + port + '...')
+    debugProd('https ', trySSL, ` listening to port ${port}...`)
   });
